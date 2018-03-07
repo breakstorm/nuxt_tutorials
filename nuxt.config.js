73c04839a6c2342810f59d334164081f66d88968
김휘겸 <breakstorm00@gmail.com>
2018-03-07 13:48:53 +0900

    Update CSS module tachyon (타키온 모듈 업데이트)

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,18 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt_tutorials',
+    title: "nuxt_tutorials",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,15 +22,22 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    // extend (config, { isDev, isClient }) {
+    //   if (isDev && isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+  },
+  css: [
+    "tachyons/css/tachyons.min.css", 
+    "tachyons-debug/css/tachyons-debug.min.css"
+  ],
+  build: {
+    vendor: ["axios"]
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Hello world</h1>
+  <h1 class="f1 code">Hello world
+    <ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>
+  {{users}}
+  </h1>
 </template>
+
+<script>
+  import axios from '~plugins/axios'
+
+  export default {
+    asyncData() {
+      return axios.get('users')
+        .then(res => ({
+          users: res.data
+        }))
+    }
+  }
+</script>
